62ed21370bf02c0486a47b3471c75c6d027ccb73
Masroor Ehsan <masroore@gmail.com>
2023-03-26 21:11:01 +0600

    remove property listing

--- smuggler/mapper.py
+++ smuggler/mapper.py
@@ -5,7 +5,7 @@ _LIST_MAP = {
     "primary_data.property_deeds": models.PropertyDeed,
     "primary_data.property_foreclosures": models.PropertyForeclosure,
     "primary_data.property_history": models.PropertyHistory,
-    "primary_data.property_listings": models.PropertyListing,
+    #"primary_data.property_listings": models.PropertyListing,
     "primary_data.property_mortgages": models.PropertyMortgage,
     "primary_data.voter_registration": models.VoterRegistrationItem,
     "primary_data.property_taxes": models.PropertyTax,
